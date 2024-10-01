a6b5f68ed5ad9de56a84e9f3bfc8bf139a2c5d57
fatou <fatouniangbasse@gmail.com>
2024-10-01 12:31:35 +0000

    first commit

---
+++ src/main/java/com/depodc/dependanceodc/DependanceOdcApplication.java
@@ -0,0 +1,13 @@
+package com.depodc.dependanceodc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependanceOdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependanceOdcApplication.class, args);
+	}
+
+}
